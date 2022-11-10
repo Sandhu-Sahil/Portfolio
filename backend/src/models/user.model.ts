d47a77c1dedadd89660a54b07e130c1de55097eb
Sandhu-Sahil <sandhu.sahil2002@gmail.com>
2022-11-10 13:22:27 +0530

    Started Backend basic code

---
+++ backend/src/models/user.model.ts
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+import { CONSTANT } from "../utils";
+
+import mongooseDelete from "mongoose-delete";
+
+const User = new mongoose.Schema({
+    nickname: {
+        type: String,
+    },
+    fullName: {
+        type: String,
+    },
+    linkedin : {
+        type:String,
+        trim: true
+    },
+    date: Date,
+    role: {
+        type: String,
+        enum: CONSTANT.USER_ROLES
+    },
+    gender: {
+        type: String,
+        enum: CONSTANT.GENDER
+    },
+    email: {
+        type: String,
+        trim: true,
+    },
+},
+    { timestamps: true }
+);
+
+User.plugin(mongooseDelete, { overrideMethods: true });
+
+export default mongoose.model("users", User);
